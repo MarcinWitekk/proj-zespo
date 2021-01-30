685bfc9a287a0aa9d6a204796ac1218667c30bc5
Marcin Witkowski <marcinwitekpl@outlook.com>
2021-01-30 15:43:26 +0100

    kursy-vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <div id="nav" class="d-flex flex-wrap ">
+      <router-link class="mr-4" to="/"><v-btn elevation="2">Home</v-btn></router-link>
+      <router-link class="mr-4" to="/productlist"><v-btn elevation="2">Product List</v-btn></router-link>
+      <router-link v-show="!loggedIn" class="mr-4" to="/login"><v-btn elevation="2">Login</v-btn></router-link>
+      <router-link v-show="!loggedIn" class="mr-4" to="/registration"><v-btn elevation="2">Registration</v-btn></router-link>
+      <router-link v-show="loggedIn" class="mr-4" to="/productmanager"><v-btn elevation="2">ProductManager</v-btn></router-link>
+      <router-link v-show="loggedIn" class="mr-4" to="/mytutorials"><v-btn elevation="2">My Tutorials</v-btn></router-link>
+      <v-btn v-show="loggedIn" v-on:click="logout()">Wyloguj</v-btn>
+    </div>      
+    <router-view/>   
+  </div> 
 </template>
+
+ <script>
+  export default {
+    data() {
+      return {
+        loggedIn: 0,
+      }
+    },
+    async created() {
+      const apiUrl = "http://25.93.216.221:8080";
+      let apiEndPoint = apiUrl + "/";
+
+    try{
+      // let response = new FormData(document.getElementById("loginForm"));
+      let response = await this.axios.get(apiEndPoint);
+        console.log("Sukces", response.data);
+        this.loggedIn = response.data;   
+        // window.location.reload()
+      } catch(e) {
+        console.log('Error: ', e);
+      }
+    },
+    methods: {
+      logout() {
+        let apiUrl = "http://25.93.216.221:8080";
+        let apiEndPoint = apiUrl + "/signOut";
+        this.axios.post(apiEndPoint, "")
+        .then(response => {
+          console.log("Sukces", response);
+          this.status2 = response.data;
+          window.location.reload()
+        })
+      }
+    }
+  }
+ </script>
 
 <style lang="less">
 #app {
@@ -17,16 +58,19 @@
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+} 
+
 #nav {
   padding: 30px;
+  border-bottom: 1px solid black;
 
   a {
     font-weight: bold;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    text-decoration: none;
   }
 }
+
 </style>
